0c5900daf8844e1eb620b163272178a79df76be0
oyas <poteto83@gmail.com>
2017-07-05 01:42:01 +0900

    fix tk error

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -4,12 +4,13 @@
 import chainer
 import chainer.functions as F
 import chainer.links as L
-from chainer.training import extensions
 from chainer.dataset import convert
 from chainer.datasets import tuple_dataset
 
 import os
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
